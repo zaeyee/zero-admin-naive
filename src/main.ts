4b2f2362054469acc594420cf538b1b35a57d619
zaeyee <zaeyee@163.com>
2023-02-09 23:28:57 +0800

    feat: login page

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import App from './App.vue'
 import router from './router'
 
-import 'normalize.css' // CSS重置方案
-import 'uno.css' // UnoCSS
-import './style.css'
+import '@unocss/reset/tailwind.css'
+import 'uno.css'
 
 const app = createApp(App)
 const pinia = createPinia()
+
+// 处理tailwind的reset样式和naive-ui中button等组件样式冲突
+const meta = document.createElement('meta')
+meta.name = 'naive-ui-style'
+document.head.appendChild(meta)
 
 app.use(router)
 app.use(pinia)
---
+++ src/views/login/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+interface LoginModel {
+  username: string
+  password: string
+}
+
+const loginModel: LoginModel = {
+  username: '',
+  password: ''
+}
+</script>
+
+<template>
+  <div fixed w-full h-full flex justify-center items-center>
+    <div class="w-240" flex shadow-2xl rounded>
+      <div class="w-60%" flex-none bg-blue p-16>
+        <img src="@/assets/bg_login.png" />
+      </div>
+      <div flex-1 p-8>
+        <div text-3xl>登录</div>
+        <n-form :model="loginModel">
+          <n-form-item>
+            <n-input v-model:value="loginModel.username" size="large" placeholder="请输入用户名" />
+          </n-form-item>
+          <n-form-item>
+            <n-input v-model:value="loginModel.password" size="large" placeholder="请输入密码" type="password" />
+          </n-form-item>
+          <n-button type="primary" attr-type="submit" size="large" block>登录</n-button>
+        </n-form>
+      </div>
+    </div>
+  </div>
+</template>
